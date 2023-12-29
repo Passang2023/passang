74c089f13dd7bf72c141f470e72e1856fc47f821
Passang2023 <155144516+Passang2023@users.noreply.github.com>
2023-12-29 15:22:25 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,56 @@
+let cards=document.querySelectorAll(".card");
+cards.forEach(function (card) { 
+    card.addEventListener("click", function () { 
+      if (card.classList.contains("open")) { 
+           card.className="card";
+         }                      
+        
+     else { 
+         card.className="card open";
+         }
+      
+      })
+  
+});
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if(n < 1) {
+        slideIndex = slides.length
+    }
+
+    for(i = 0; i < slides.length; i++) {
+        slides [i].style.display = "none";
+    }
+
+    for (i=0; i < dots.length; i++){
+        dots [i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides [slideIndex-1].style.display = "block"; 
+
+    dots[slideIndex-1].className += " active";
+}
+
+function press(){
+    alert('successfully send');
+}
